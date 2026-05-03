177d280b0af47ecdab4772935ebfcd4a710d16a7
Asma-Munni <asmahossain529@gmail.com>
2026-05-03 22:08:33 +0600

    Added

--- src/app/(main)/products/page.jsx
+++ src/app/(main)/products/page.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import React from 'react';
 
 async function getProducts  () {
-const res = await fetch("http://localhost:3000/products.json");
+const res = await fetch("http://localhost:3000/products.json",{cache:"no-store"});
 const data = await res.json();
 return data.products;
 
